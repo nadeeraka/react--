35b3c31892b0fc42a8bf3fcd354dedc58f40483b
nadeeraka <nadeerakan@gmail.com>
2019-11-23 01:31:08 +0530

    types added

--- hey-ts/src/components/logic.tsx
+++ hey-ts/src/components/logic.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Form } from "./form";
-import { JSXElement } from '@babel/types';
+import { string } from 'prop-types';
+type FormEl = React.FormEvent<HTMLFormElement>
+
+interface ITodo {
+    text:string
+    completed: boolean
+}
 
 export const Logic = ():JSX.Element => {
+    const [value, setvalue] = useState<string>('');
+    const [todos, settodos] = useState<ITodo[]>([]);
+    const handelChange =(e:FormEl)=>{
+        setvalue('')
+    }
     return (
         <div>
-            <Form/>
+            <form action="">
+                <input type="text" onChange={e=>setvalue(e.target.value)}  value={value} name=""  id=""/>
+            </form>
         </div>
     )
 }
