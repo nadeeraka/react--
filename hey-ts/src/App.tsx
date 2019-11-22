3e7f6bfc235ae5442006f8cca7e0e8bad2714ef6
nadeeraka <nadeerakan@gmail.com>
2019-11-23 00:36:50 +0530

    3 commit

---
+++ hey-ts/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { Logic } from "./components/logic";
+
+function App():JSX.Element {
+  return (
+    <div className="App">
+      <Logic/>
+    </div>
+  );
+}
+
+export default App;
---
+++ hey-ts/src/components/logic.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Form } from "./form";
+import { JSXElement } from '@babel/types';
+
+export const Logic = ():JSX.Element => {
+    return (
+        <div>
+            <Form/>
+        </div>
+    )
+}
