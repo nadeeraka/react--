7734edc5b0e279b9e2fa255b6238f257cc0a1b71
nadeeraka <nadeerakan@gmail.com>
2019-11-23 10:32:20 +0530

    logic added

--- hey-ts/src/components/logic.tsx
+++ hey-ts/src/components/logic.tsx
@@ -12,12 +12,24 @@ export const Logic = ():JSX.Element => {
     const [value, setvalue] = useState<string>('');
     const [todos, settodos] = useState<ITodo[]>([]);
     const handelChange =(e:FormEl)=>{
+        e.preventDefault()
+        addTodo(value)
+        
         setvalue('')
+    }
+
+    const addTodo=(text:string)=>{
+        let newArray:ITodo[] = [...todos,{text,completed:false}] 
+      settodos(newArray);
     }
     return (
         <div>
-            <form action="">
+            <form action="" onSubmit={handelChange}>
                 <input type="text" onChange={e=>setvalue(e.target.value)}  value={value} name=""  id=""/>
+                <button type='submit'>Submit</button>
+                {todos.map((data:ITodo,index:number)=>{
+                return <p key={index}>{data.text}</p>
+                })}
             </form>
         </div>
     )
